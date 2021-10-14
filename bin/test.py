fe1f788cfc4e6a63a7bf9170a079bfde323507fb
Michal Dupkala <mdupdevelop@gmail.com>
2021-10-14 18:17:57 +0200

    Redundant code

--- bin/test.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import os
-
-with open(r'./conf.yaml') as file:
-    conf_yaml = yaml.safe_load(file)
-
-    conf_dir_name        = conf_yaml['dir_name']
-
-os.mkdir(f'./{conf_dir_name}')
