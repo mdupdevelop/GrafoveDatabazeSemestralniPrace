4e20ee686774276faecc6cccf08b14c7edc13c92
Michal Dupkala <mdupdevelop@gmail.com>
2021-11-23 10:06:30 +0100

    Renamed containers in tkinter

--- bin/main.py
+++ bin/main.py
@@ -33,47 +33,47 @@ if __name__ == '__main__':
     center.grid_rowconfigure(0, weight=1)
     center.grid_columnconfigure(1, weight=1)
 
-    left_column = tk.Frame(center, width = 250)
-    right_column = tk.Frame(center, width = 250)
+    top_row = tk.Frame(center, width = 250)
+    bottom_row = tk.Frame(center, width = 250)
 
-    left_column.grid(row = 0, column = 0, sticky = "ew")
-    right_column.grid(row = 1, column = 0, sticky = "ew")
+    top_row.grid(row = 0, column = 0, sticky = "ew")
+    bottom_row.grid(row = 1, column = 0, sticky = "ew")
     
     # Left column buttons
-    tk.Label(left_column, text = 'Neo4j server settings').grid(row = 0, column =    0, columnspan = 3, sticky = tk.W + tk.E)
+    tk.Label(top_row, text = 'Neo4j server settings').grid(row = 0, column =    0, columnspan = 3, sticky = tk.W + tk.E)
 
-    tk.Label(left_column, text = 'Server:', width= 15).grid(row = 1, column = 0)
-    server_entry = tk.Entry(left_column)
+    tk.Label(top_row, text = 'Server:', width= 15).grid(row = 1, column = 0)
+    server_entry = tk.Entry(top_row)
     server_entry.insert(0, 'bolt://localhost:7687')
     server_entry.grid(row = 1, column = 1)
 
-    tk.Label(left_column, text = 'Username:').grid(row = 2, column = 0)
-    username_entry = tk.Entry(left_column)
+    tk.Label(top_row, text = 'Username:').grid(row = 2, column = 0)
+    username_entry = tk.Entry(top_row)
     username_entry.insert(0, 'neo4j')
     username_entry.grid(row = 2, column = 1)
 
-    tk.Label(left_column, text = 'Password').grid(row = 3, column = 0)
-    password_entry = tk.Entry(left_column)
+    tk.Label(top_row, text = 'Password').grid(row = 3, column = 0)
+    password_entry = tk.Entry(top_row)
     password_entry.insert(0, '123')
     password_entry.grid(row = 3, column = 1)
 
-    tk.Button(left_column, text = "Clear DB", command = lambda: [yaml_editor.edit_conf_graph(server_entry.get(), username_entry.get(), password_entry.get()), graphtransfer.clear_db()], width= 10).grid(row = 1, column = 2, rowspan = 3, sticky = tk.N + tk.S)
+    tk.Button(top_row, text = "Clear DB", command = lambda: [yaml_editor.edit_conf_graph(server_entry.get(), username_entry.get(), password_entry.get()), graphtransfer.clear_db()], width= 10).grid(row = 1, column = 2, rowspan = 3, sticky = tk.N + tk.S)
 
 
     # Right column buttons
-    tk.Label(right_column, text = 'Page settings').grid(row = 0, columnspan = 3, sticky = tk.W + tk.E)
+    tk.Label(bottom_row, text = 'Page settings').grid(row = 0, columnspan = 3, sticky = tk.W + tk.E)
 
-    tk.Label(right_column, text = 'Folder name:', width= 15).grid(row = 1, column = 0)
-    tk.Entry(right_column).grid(row = 1, column = 1)
+    tk.Label(bottom_row, text = 'Folder name:', width= 15).grid(row = 1, column = 0)
+    tk.Entry(bottom_row).grid(row = 1, column = 1)
 
-    tk.Label(right_column, text = 'Article URL:').grid(row = 2, column = 0)
-    tk.Entry(right_column).grid(row = 2, column = 1)
+    tk.Label(bottom_row, text = 'Article URL:').grid(row = 2, column = 0)
+    tk.Entry(bottom_row).grid(row = 2, column = 1)
 
-    tk.Label(right_column, text = 'Pages to scrape:').grid(row = 3, column = 0)
-    tk.Entry(right_column).grid(row = 3, column = 1)
+    tk.Label(bottom_row, text = 'Pages to scrape:').grid(row = 3, column = 0)
+    tk.Entry(bottom_row).grid(row = 3, column = 1)
 
-    tk.Button(right_column, text = "Scrape", command = lambda: [yaml_editor.edit_conf_files(), scraper.main()], width= 10).grid(row = 1, column = 2)
-    tk.Button(right_column, text = "Send to graph", command = lambda: [yaml_editor.edit_conf_files(), graphtransfer.main()], width= 10).grid(row = 2, column = 2)
-    tk.Button(right_column, text = "Both", command = lambda: [yaml_editor.edit_conf_files(), scraper.main(), graphtransfer.main()], width= 10).grid(row = 3, column = 2)
+    tk.Button(bottom_row, text = "Scrape", command = lambda: [yaml_editor.edit_conf_files(), scraper.main()], width= 10).grid(row = 1, column = 2)
+    tk.Button(bottom_row, text = "Send to graph", command = lambda: [yaml_editor.edit_conf_files(), graphtransfer.main()], width= 10).grid(row = 2, column = 2)
+    tk.Button(bottom_row, text = "Both", command = lambda: [yaml_editor.edit_conf_files(), scraper.main(), graphtransfer.main()], width= 10).grid(row = 3, column = 2)
 
     root.mainloop()
